c7891856e4d0c264bf655c7708afdf2170485f82
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-04-12 10:08:25 +0200

    Update panic.asm

--- kernel/panic.asm
+++ kernel/panic.asm
@@ -4,13 +4,13 @@
 ; PANIC.ASM may only be ran if a fatal kernel error occurs.
 
 panic:
-push si
-mov si, panicstr1
-call printstring
-pop si
-call printstring
-mov si, panicstr2
-call printstring
+    push si             ; Push the message to the stack
+    mov si, panicstr1
+    call printstring
+    pop si              ; Pop the message so we can use it
+    call printstring
+    mov si, panicstr2
+    call printstring
 jmp $
 
 panicstr1 db 13,10,"Kernel Panic: ", 0
