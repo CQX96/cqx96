3f5dc4b230a80211a0bd5dadf6b9ebdf78b8f490
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-05-07 14:43:39 +0200

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -9,6 +9,26 @@
 8 bits(al) lower :
   8 bits : ASCII character to print
 */
+void kernel_entry()
+{
+  //first init vga with fore & back colors
+  init_vga(WHITE, BLACK);
+
+  //assign each ASCII character to video buffer
+  //you can change colors here
+  vga_buffer[0] = vga_entry('H', WHITE, BLACK);
+  vga_buffer[1] = vga_entry('e', WHITE, BLACK);
+  vga_buffer[2] = vga_entry('l', WHITE, BLACK);
+  vga_buffer[3] = vga_entry('l', WHITE, BLACK);
+  vga_buffer[4] = vga_entry('o', WHITE, BLACK);
+  vga_buffer[5] = vga_entry(' ', WHITE, BLACK);
+  vga_buffer[6] = vga_entry('W', WHITE, BLACK);
+  vga_buffer[7] = vga_entry('o', WHITE, BLACK);
+  vga_buffer[8] = vga_entry('r', WHITE, BLACK);
+  vga_buffer[9] = vga_entry('l', WHITE, BLACK);
+  vga_buffer[10] = vga_entry('d', WHITE, BLACK);
+}
+
 uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color) 
 {
   uint16 ax = 0;
@@ -39,24 +59,4 @@ void init_vga(uint8 fore_color, uint8 back_color)
 {
   vga_buffer = (uint16*)VGA_ADDRESS;  //point vga_buffer pointer to VGA_ADDRESS 
   clear_vga_buffer(&vga_buffer, fore_color, back_color);  //clear buffer
-}
-
-void kernel_entry()
-{
-  //first init vga with fore & back colors
-  init_vga(WHITE, BLACK);
-
-  //assign each ASCII character to video buffer
-  //you can change colors here
-  vga_buffer[0] = vga_entry('H', WHITE, BLACK);
-  vga_buffer[1] = vga_entry('e', WHITE, BLACK);
-  vga_buffer[2] = vga_entry('l', WHITE, BLACK);
-  vga_buffer[3] = vga_entry('l', WHITE, BLACK);
-  vga_buffer[4] = vga_entry('o', WHITE, BLACK);
-  vga_buffer[5] = vga_entry(' ', WHITE, BLACK);
-  vga_buffer[6] = vga_entry('W', WHITE, BLACK);
-  vga_buffer[7] = vga_entry('o', WHITE, BLACK);
-  vga_buffer[8] = vga_entry('r', WHITE, BLACK);
-  vga_buffer[9] = vga_entry('l', WHITE, BLACK);
-  vga_buffer[10] = vga_entry('d', WHITE, BLACK);
 }
