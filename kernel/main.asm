88e5d34e149401280af10b94ebab11c79af0ec60
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2022-06-13 19:18:58 +0200

    New kernel API (also 96th commit)

--- kernel/main.asm
+++ kernel/main.asm
@@ -61,7 +61,8 @@ jmp not_implemented	  ;0054h
 ;	NONE
 jmp os_get_file_list      ;0057h [get a list of files]
 jmp change_device         ;005Ah [change current device number, AX=new number]
-jmp get_device            ;005Ah [get current device number, SI=number]
+jmp get_device            ;005Dh [get current device number, SI=number]
+jmp os_string_parse       ;0060h [parse into ax,bx,cx,dx, SI=what to parse]
 disk_buff	equ	24576
 cqx:
 	cli
